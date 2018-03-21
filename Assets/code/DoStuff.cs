1f254eeabf7ccbd9e5656722374cd9af49a05491
Chad Frederick <freedomrock1@gmail.com>
2018-03-21 12:00:32 -0400

    Init
    Initial commit

---
+++ Assets/code/DoStuff.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoStuff : MonoBehaviour {
+	public Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pos = new Vector3 (0.0f, 1.0f, 0.0f);
+		Vector3 force = new Vector3 (0.0f, 0.0f, 0.0f);
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			rb.MovePosition (pos);
+			rb.velocity = force;
+			rb.angularVelocity = force;
+		}
+	}
+	// called just before perfoming any physics
+	void FixedUpdate() {
+		//Vector3 force=(10,10,0);
+		float mh=Input.GetAxis ("Horizontal");
+		float mv = Input.GetAxis ("Vertical");
+
+		
+
+	}
+}
---
+++ Assets/code/DoStuffZero.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoStuffZero : MonoBehaviour {
+	public float speed;
+	private Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	// called just before perfoming any physics
+	void FixedUpdate() {
+		//Vector3 force=(10,10,0);
+		float mh=Input.GetAxis ("Horizontal");
+		float mv = Input.GetAxis ("Vertical");
+
+		Vector3 force=new Vector3(mh,0,mv);
+
+		rb.AddForce (force*speed);
+
+	}
+}
